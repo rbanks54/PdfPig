db567af1c8d326a39ad3e6e96135c4451c278a2f
Eliot Jones <elioty@hotmail.co.uk>
2019-05-26 14:33:37 +0100

    add classes to subset truetype fonts prior to embedding

---
+++ src/UglyToad.PdfPig.Tests/Fonts/TrueType/Embedding/TrueTypeSubsetterTests.cs
@@ -0,0 +1,32 @@
+﻿namespace UglyToad.PdfPig.Tests.Fonts.TrueType.Embedding
+{
+    using System;
+    using System.IO;
+    using PdfPig.Fonts.TrueType;
+    using PdfPig.Fonts.TrueType.Embedding;
+    using PdfPig.Fonts.TrueType.Parser;
+    using PdfPig.IO;
+    using Xunit;
+
+    public class TrueTypeSubsetterTests
+    {
+        private readonly TrueTypeFontParser parser = new TrueTypeFontParser();
+
+        [Fact]
+        public void CanCreateValidRobotoSubset()
+        {
+            var file = GetFontFile("Roboto-Regular.ttf");
+
+            var font = parser.Parse(new TrueTypeDataBytes(new ByteArrayInputBytes(file)));
+              
+            TrueTypeSubsetter.Subset(font, new int[]{ 'H', 'e', 'l', 'o', 'w', 'r', 'd' });
+        }
+
+        private static byte[] GetFontFile(string name)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fonts", "TrueType");
+
+            return File.ReadAllBytes(Path.Combine(path, name));
+        }
+    }
+}
---
+++ src/UglyToad.PdfPig/Fonts/TrueType/Embedding/TrueTypeSubsetter.cs
@@ -0,0 +1,44 @@
+﻿namespace UglyToad.PdfPig.Fonts.TrueType.Embedding
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using Tables;
+
+    internal class TrueTypeSubsetter
+    {
+        private static readonly byte[] VersionHeader =
+        {
+            0, 1, 0, 0
+        };
+
+        public static void Subset(TrueTypeFontProgram font, IReadOnlyList<int> codePoints)
+        {
+            // Find the corresponding glyph,
+            // Keep the normal header
+            // Write the glyphs
+            // Write the index to location table
+
+            // Required
+            var glyf = font.TableRegister.GlyphTable;
+            var head = font.TableRegister.HeaderTable;
+            var hhea = font.TableRegister.HorizontalHeaderTable;
+            var hmtx = font.TableRegister.HorizontalMetricsTable;
+            var loca = font.TableRegister.IndexToLocationTable;
+            var maxp = font.TableRegister.MaximumProfileTable;
+
+
+            using (var stream = new MemoryStream())
+            {
+                // TODO: write header.
+                // stream.Write(VersionHeader, 0, VersionHeader.Length);
+
+
+            }
+        }
+
+        private static void WriteHeadTable(HeaderTable header)
+        {
+
+        }
+    }
+}
