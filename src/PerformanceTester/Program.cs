25cc438eb2b36027e6f05fdf0aa9665aff575514
Richard Banks <rbanks54@msn.com>
2019-07-23 12:06:01 +1000

    add benchmarks

--- src/PerformanceTester/Program.cs
+++ src/PerformanceTester/Program.cs
@@ -9,14 +9,14 @@ namespace PerformanceTester
         static void Main(string[] args)
         {
             var iliadPath = @"C:\pd\pdfpig\pdfs\homers illiad.pdf";
-            var warAndPeacePath = @"C:\pd\pdfpig\pdfs\War and Peace.pdf";
+            // var warAndPeacePath = @"C:\pd\pdfpig\pdfs\War and Peace.pdf";
 
 
             var tests = new Dictionary<string, string[]>
             {
                 { "just the iliad", new string[]{iliadPath} },
-                { "just war and peace", new string[]{warAndPeacePath} },
-                { "both", new string[]{iliadPath, warAndPeacePath} },
+                // { "just war and peace", new string[]{warAndPeacePath} },
+                // { "both", new string[]{iliadPath, warAndPeacePath} },
             };
 
             foreach (var test in tests)
